5cce4814906c70b63ab56c28814c00bb6b498afe
Eugênio Freire <eugenio9008@gmail.com>
2020-07-12 16:44:59 -0300

    projeto concluido

--- components/Link.js
+++ components/Link.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Link = ({ href, Children }) => {
-  return <a href={href}className='bg-gray-800'>{Children}</a>
+const Link = ({ href, children }) => {
+  return <a href={href}className='bg-blue-700 py-4 w-full block text-center hover:bg-blue-200 hover:text-black border-solid border-2 my-4 border-blue-700 text-white hover:shadow-lg rounded-md'>{children}</a>
 }
 
 export default Link
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import Link from '../components/Link'
+import Prismic from 'prismic-javascript'
 
-const Index = () => {
+const Index = props => {
+  const items = props.data.body[0].items
+
   return (
-    <div>
-      <h1>Eugênio Carvalho</h1>
+    <div className=' max-w-md mx-auto p-2'>
+      <h1>
+        <img className='mx-auto rounded-full h-32' src={props.data.headerimage.url} alt='Eugênio Carvalho'/>
+      </h1>
+      <h2 className='text-center p-6 text-gray-700 italic font-bold'>Eugênio Carvalho</h2>
       <ul>
-        <li>
-          <Link href='aa'>AAA</Link>
-        </li>
-        <li>Link 2</li>
-        <li>Link 3</li>
-        <li>Link 4</li>
-        <li>Link 5</li>
-        <li>Link 6</li>
+        {items.map(item => {
+          return (
+            <li>
+              <Link href={item.link.url}>{item.text}</Link>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const client = Prismic.client('https://linktree-clone.cdn.prismic.io/api/v2')
+
+  const page = await client.getSingle('home')
+
+  return {
+    props: {
+      data: page.data
+    }
+  }
 }
 
 export default Index
